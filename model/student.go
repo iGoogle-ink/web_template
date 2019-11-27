d48319ab25f034f95df5c847a5395e8a5156ba44
Jerry <fumingming@bilibili.com>
2019-11-27 20:00:56 +0800

    update

--- model/student.go
+++ model/student.go
@@ -3,16 +3,19 @@ package model
 import "time"
 
 type Student struct {
-	Id    int       `json:"id"`
-	Name  string    `json:"name"`
-	Ctime time.Time `json:"ctime"`
-	Mtime time.Time `json:"mtime"`
+	Id         int       `json:"id"`
+	Name       string    `json:"name"`
+	CreateTime int64     `json:"ctime" xorm:"-"`
+	UpdateTime int64     `json:"mtime" xorm:"-"`
+	Ctime      time.Time `json:"-"`
+	Mtime      time.Time `json:"-"`
 }
 
 type StudentListRsp struct {
 	StudentList []*Student `json:"student_list"`
 }
 
-func (s *Student) TimeToUnix() {
-
+func (s *Student) AfterLoad() {
+	s.CreateTime = s.Ctime.Unix()
+	s.UpdateTime = s.Mtime.Unix()
 }
