8c7037dca78e7af56c1b20531ec6db453cfb0cc6
Jerry <fumingming@bilibili.com>
2019-12-03 19:37:27 +0800

    update

--- pkg/time.go
+++ pkg/time.go
@@ -1,4 +1,35 @@
 package pkg
 
+import (
+	"database/sql/driver"
+	"fmt"
+	"strconv"
+	"time"
+)
+
 // Time be used to MySql timestamp converting.
 type Time int64
+
+// Scan scan time.
+func (t *Time) Scan(src interface{}) (err error) {
+	switch sc := src.(type) {
+	case time.Time:
+		*t = Time(sc.Unix())
+	case string:
+		var i int64
+		i, err = strconv.ParseInt(sc, 10, 64)
+		fmt.Println("Scan Err:", err)
+		*t = Time(i)
+	}
+	return
+}
+
+// Value get time value.
+func (t Time) Value() (driver.Value, error) {
+	return time.Unix(int64(t), 0), nil
+}
+
+// Time get time.
+func (t Time) Time() time.Time {
+	return time.Unix(int64(t), 0)
+}
