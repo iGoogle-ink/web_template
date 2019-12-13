5f88c91a7319d4c1bdfba411fa70e165f5562ba5
jerry <85411418@qq.com>
2019-12-13 23:45:41 +0800

    update

---
+++ dao/school/binding_test.go
@@ -0,0 +1,28 @@
+package school
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDao_GetBindings(t *testing.T) {
+	list, err := d.GetBindings(1)
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+	for _, b := range list {
+		fmt.Println("binding:", b)
+	}
+}
+
+func TestDao_GetBindingsByPid(t *testing.T) {
+	list, err := d.GetBindingsByPid(1, 2)
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+	for _, b := range list {
+		fmt.Println("binding:", b)
+	}
+}
