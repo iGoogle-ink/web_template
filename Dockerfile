bb2378edadea713ced87a463b227c2b44d90ab66
Jerry <mingming.fu@sunmi.com>
2020-08-21 12:04:19 +0800

    new

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-FROM busybox
-MAINTAINER Jerry "jerry@igoogle.ink"
-COPY application /usr/application
-COPY cmd/web_template.json /usr/web_template.json
-CMD ["/usr/application", "-env", "prod", "-conf", "/usr/web_template.json"]
+FROM alpine
+MAINTAINER Jerry "jerry@gopay.ink"
+COPY main /usr/bin/main
 EXPOSE 80
 
+# CMD ["main", "-conf", "/config/vbs9010_app_api.yaml"]
 # docker run -d --restart=unless-stopped -v /etc/localtime:/etc/localtime:ro -p 2233:2233 --name=web_template web_template:latest
-# /usr/application -env test -conf /usr/web_template.json
--- build.sh
+++ build.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-
 function build_docker {
 #   $1 代表第一个参数
     TAG=$1
+    if [[ -z "$TAG" ]]; then
+        echo "image tag can not null"
+        exit
+    fi
 
     IMAGE_NAME="web_template"
 
     IMAGE_FULL_NAME="$IMAGE_NAME:$TAG"
 
-    HAS_OLD_IMAGES=$(docker images|grep $IMAGE_NAME|grep $TAG|wc -l)
-    echo $HAS_OLD_IMAGES
+    echo ${IMAGE_FULL_NAME}
 
-    if [ $HAS_OLD_IMAGES -ne "0" ]; then
+    HAS_OLD_IMAGES=$(docker images|grep ${IMAGE_NAME}|grep ${TAG}|wc -l)
+    echo ${HAS_OLD_IMAGES} ${IMAGE_FULL_NAME} "images"
+
+    if [[ ${HAS_OLD_IMAGES} -ne "0" ]]; then
         echo "Remove docker image..."
-        docker rmi -f $IMAGE_FULL_NAME
+        docker rmi -f ${IMAGE_FULL_NAME}
     fi
 
     echo "Building docker image..."
-    docker build -t $IMAGE_FULL_NAME .
+    docker build -t ${IMAGE_FULL_NAME} .
+
+    # docker push ${IMAGE_FULL_NAME}
 }
 
 set -e
@@ -26,12 +33,14 @@ echo "Building application..."
 
 go mod tidy
 
-CGO_ENABLED=0 GOOS=linux go build -o application cmd/main.go
+CGO_ENABLED=0 GOOS=linux go build cmd/main.go
 
-build_docker "latest"
+build_docker $1
 
-echo "Cleanup application..."
+echo "Cleanup..."
 
-rm application
+rm main
+
+docker image prune -f
 
 echo "Done"
